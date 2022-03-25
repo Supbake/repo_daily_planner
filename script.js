4bc0a0635da5a0780a08a6d21321dae6a3146ff2
jacob ramirez <jacobnramirez@gmail.com>
2022-03-24 19:43:39 -0700

    added the ability to save the input field into local storage

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-$(document).ready(function() {
+$(function() { ///date and time 
     function displayDate() {
         $("#currentDay").html(moment().format("LLLL"));
        }
        setInterval(displayDate, 1000);
 })
 
+$(".saveButton").click(function () {
+    let input = $(this).siblings(".input-plans").val();
+    let hour = $(this).siblings(".input-plans").attr("id");
+    localStorage.setItem(input, hour);
+})
 
+
+$(function() { ///stored inputs for "input-plans"
+    function currentPlans() {
+        const inputPlans = $(".input-plans")
+        $("").val(localStorage.getItem(""))
+    }
+})
