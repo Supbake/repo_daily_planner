1a771377e7d37f4835a2a22011aae5d5142a6085
jacob ramirez <jacobnramirez@gmail.com>
2022-03-24 20:46:10 -0700

    added the ability to pull inputs from local storage and added button to clear the day

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
+const eightAM = document.querySelector("#\\38 ");
+const nineAM = document.querySelector("#\\39 ");
+const tenAM = document.querySelector("#\\31 0");
+const elevenAM = document.querySelector("#\\31 1");
+const twelvePM = document.querySelector("#\\31 2");
+const onePM = document.querySelector("#\\31 3");
+const twoPM = document.querySelector("#\\31 4");
+const threePM = document.querySelector("#\\31 5");
+const fourPM = document.querySelector("#\\31 6");
+const fivePM = document.querySelector("#\\31 7");
+const sixPM = document.querySelector("#\\31 8");
+const sevenPM = document.querySelector("#\\31 9");
+const eightPM = document.querySelector("#\\32 0");
+
+
+const inputArry = [eightAM, nineAM, tenAM, elevenAM, twelvePM, onePM, twoPM, threePM, fourPM, fivePM, sixPM, sevenPM, eightPM];
+
 $(function() { ///date and time 
+    console.log("this will run first");
     function displayDate() {
         $("#currentDay").html(moment().format("LLLL"));
        }
        setInterval(displayDate, 1000);
+    // eightAM.value = localStorage.getItem("8");
+    for (let i = 0; i < inputArry.length; i++) {
+        inputArry[i].value = localStorage.getItem(`${i + 8}`);
+    }
+    console.log(eightAM);
 })
 
 $(".saveButton").click(function () {
-    let input = $(this).siblings(".input-plans").val();
-    let hour = $(this).siblings(".input-plans").attr("id");
-    localStorage.setItem(input, hour);
+    const input = $(this).siblings(".input-plans").val();
+    const hour = $(this).siblings(".input-plans").attr("id");
+    localStorage.setItem(hour, input);
+    
 })
 
-
-$(function() { ///stored inputs for "input-plans"
-    function currentPlans() {
-        const inputPlans = $(".input-plans")
-        $("").val(localStorage.getItem(""))
-    }
+$(".clearButton").click(function () {
+    localStorage.clear();
+    location.reload();
 })
+
